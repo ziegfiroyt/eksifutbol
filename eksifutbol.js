84a9c03b09fe00e444f8281ce828cd04be2eb167
ziegfiroyt <sozenugur@gmail.com>
2014-08-15 15:10:56 +0300

    Create eksifutbol.js

---
+++ eksifutbol.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name	eksifutbol
+// @namespace	https://github.com/ziegfiroyt/eksifutbol
+// @author	ziegfiroyt @github
+// @description	futbol bizim isimiz
+// @match	https://eksisozluk.com/*
+// @match	https://*.eksisozluk.com/*
+// @version	0.7
+// @grant       none
+// ==/UserScript==
+
+
+$(document).ready(function() {
+    
+   var zieg = $('h1#title span[itemprop="name"]').text();
+
+    $('#aside').prepend('<iframe width="300" height="250" scrolling="no" frameborder="no" src="https://www.tribundergi.com/eksifutbol.php?q='+zieg+'"></iframe>');
+    $('#quick-index-nav').append('<li><a class="ara" href="ara?searchform.keywords=maçı&searchform.sortorder=date&p=1">maçlar</a></li>');
+	
+});
