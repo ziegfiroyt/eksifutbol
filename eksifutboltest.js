75db9fff0981df0aae8e3d0a16ba5abfd0ef906b
ziegfiroyt <sozenugur@gmail.com>
2014-08-15 14:55:31 +0300

    Create eksifutboltest.js

---
+++ eksifutboltest.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name	eksi futbol test
+// @namespace	https://github.com/ziegfiroyt/eksifutbol
+// @author	ziegfiroyt @github
+// @description	futbol bizim isimiz
+// @match	https://eksisozluk.com/*
+// @match	https://*.eksisozluk.com/*
+// @version	0.7
+// @grant none
+// ==/UserScript==
+
+/* selam */
