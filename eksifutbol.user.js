6fcaaabb9158eb90ae3ec4186a0bf362a77c7168
ziegfiroyt <sozenugur@gmail.com>
2014-08-22 16:18:01 +0300

    Update eksifutbol.user.js

--- eksifutbol.user.js
+++ eksifutbol.user.js
@@ -9,12 +9,14 @@
 // @grant       none
 // ==/UserScript==
 
+// selam beyler nasilsiniz?
 
 $( document ).ready(function() {
-    
-   var zieg = $('h1#title span[itemprop="name"]').text();
 
-    $('#aside').prepend('<iframe width="300" height="250" scrolling="no" frameborder="no" src="https://www.tribundergi.com/eksifutbol.php?q='+zieg+'"></iframe>');
-    $('#quick-index-nav').append('<li><a class="ara" href="ara?searchform.keywords=maçı&searchform.sortorder=date&p=1">maçlar</a></li>');
-	
+var zieg = $('h1#title span[itemprop="name"]').text();
+
+$('#aside').prepend('<iframe width="300" height="250" id="eksifutbol" scrolling="no" frameborder="no" src="https://www.tribundergi.com/eksifutbol.php?q='+zieg+'"></iframe>');
+$('#quick-index-nav').append('<li><a class="ara" href="ara?searchform.keywords=maçı&searchform.sortorder=date&p=1">maçlar</a></li>');
+$('.aside-link').prepend('<a href="#eksifutbol" class="more-data">ekşi futbol</a>');	
+
 });
